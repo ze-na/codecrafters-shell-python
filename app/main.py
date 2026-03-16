79a23d0848f2e1e75f95f6126abde94adca320bb
Zena <zena.elghlan@hotmail.com>
2026-03-16 17:18:13 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if (command == "exit"):
+            break
         print(f"{command}: command not found")
 
 
