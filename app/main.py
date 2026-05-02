c95bc0906e51dd233ea3f8884bbbcece752a671f
Zena <zena.elghlan@hotmail.com>
2026-05-02 15:46:49 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 import sys
 
+commands = {
+    "exit": lambda line: sys.exit(0),
+    "echo": lambda line: print(line[1:]),
+    "type": lambda line: print(f"{args} is a shell builtin")
+    if (args := "".join(line.split()[1:])) in commands
+    else print(f"{args}: not found"),
+}
+
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if (command == "exit"):
-            break
-        elif command.startswith("echo "):
-            sys.stdout.write(command[5:] + "\n")
-        else: 
-            print(f"{command}: command not found")
+
+        line = input()
+
+        for command in commands:
+            if line.startswith(command):
+                commands[command](line)
+                break
+        else:
+            sys.stderr.write(f"{line}: command not found\n")
 
 
 if __name__ == "__main__":
