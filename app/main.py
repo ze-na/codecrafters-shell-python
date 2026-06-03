70f365f030fb19d60f00d655cb89a23980dcee81
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-03 21:43:42 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,29 +1,36 @@
 import sys
-import shutil
+import os
+
+BUILDINS = ["exit", "echo", "type"]
+PATH = os.environ.get("PATH", "")
+PATHSEP = os.pathsep
 
 
 def main():
-    builtin = ["echo", "exit", "type"]
-
+    
+    print(PATH)
+    
     while True:
         sys.stdout.write("$ ")
-
-        command: str = input()
+        command = input()
         if command == "exit":
             break
-
         elif command.startswith("echo "):
-            print(command.removeprefix("echo "))
-
+            print(command[5:])
         elif command.startswith("type "):
-            bic: str = command.removeprefix("type ")
-            if bic in builtin:
-                print(f"{bic} is a shell builtin")
-            elif shutil.which(bic):
-                print(f"{bic} is {shutil.which(bic)}")
+            cmd = command[5:]
+            if cmd in BUILDINS:
+                print(f"{cmd} is a shell builtin")
+            elif PATH:
+                for dir in PATH.split(os.pathsep):
+                    full_path = os.path.join(dir, cmd)
+                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                        print(f"{cmd} is {full_path}")
+                        break
+                else:
+                    print(f"{cmd}: not found")
             else:
-                print(f"{bic}: not found")
-
+                print(f"{cmd}: not found")
         else:
             print(f"{command}: command not found")
 
