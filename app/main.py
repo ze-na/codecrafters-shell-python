9039812439805996c1149277925a41d690eb5f7c
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-04 18:58:11 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def find_dir(program):
     #program_path = os.pathsep
     #look through path from left to right to find program
     PATH = os.environ['PATH']
-    print(f"test, PATH = {PATH}")
+    # print(f"test, PATH = {PATH}")
     directories = PATH.split(os.pathsep) # split with path separater
     
     # for x in directories:
