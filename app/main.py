c4cb4b03739bdb5605374eb48ed7f5a13655c300
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-04 19:00:49 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,38 +6,20 @@ commands = {
     "echo": lambda line: print(line[5:]),
     "type": lambda line: print(f"{args} is a shell builtin")
     if (args := "".join(line.split()[1:])) in commands
-    else find_dir(line[5:]) #print(f"{args}: not found") 
+    else find_dir(line[5:])
 }
 
 def find_dir(program):
-    #todo: remember the path to get to program.
-    #program_path = os.pathsep
-    #look through path from left to right to find program
-    PATH = os.environ['PATH']
-    # print(f"test, PATH = {PATH}")
-    directories = PATH.split(os.pathsep) # split with path separater
-    
-    # for x in directories:
-    #     print(f"test, directories = {x}")
-        
-    # if (program in directories): # check if program is in PATH and #execute permission boolean
-    #     print(f"test, program = {program} programPath = {programPATH}")
+    #split path in directories
+    directories = os.environ['PATH'].split(os.pathsep) # split with path separater
+
     for directory_path in directories:
-        # if (x == program):
-        #     break
         program_path = os.path.join(directory_path, program)
         
-      #  program_path = program_path + os.pathsep + directoryPath
-      #  program_path = os.path.join(programPATH, directory_path)
-    
         if os.path.exists(program_path) and os.access(program_path, os.X_OK):
             print(f"{program} is {program_path}")
             return
-        # else:
-        #     print(f"{program}: not found")
-        #     exit()
     else: 
-        # print(f"test, program = {program} flop ")
         print(f"{program}: not found")
         
 
