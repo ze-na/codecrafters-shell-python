52d18c926e45b46e2f822a68501261c8c47b3628
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-04 18:55:45 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,38 +1,57 @@
 import sys
 import os
-import time
 
-BUILDINS = ["exit", "echo", "type"]
-PATH = os.environ.get("PATH", "")
-PATHSEP = os.pathsep
+commands = {
+    "exit": lambda line: sys.exit(0),
+    "echo": lambda line: print(line[5:]),
+    "type": lambda line: print(f"{args} is a shell builtin")
+    if (args := "".join(line.split()[1:])) in commands
+    else find_dir(line[5:]) #print(f"{args}: not found") 
+}
+
+def find_dir(program):
+    #todo: remember the path to get to program.
+    #program_path = os.pathsep
+    #look through path from left to right to find program
+    PATH = os.environ['PATH']
+    print(f"test, PATH = {PATH}")
+    directories = PATH.split(os.pathsep) # split with path separater
+    
+    # for x in directories:
+    #     print(f"test, directories = {x}")
+        
+    # if (program in directories): # check if program is in PATH and #execute permission boolean
+    #     print(f"test, program = {program} programPath = {programPATH}")
+    for directory_path in directories:
+        # if (x == program):
+        #     break
+        program_path = os.path.join(directory_path, program)
+        
+      #  program_path = program_path + os.pathsep + directoryPath
+      #  program_path = os.path.join(programPATH, directory_path)
+    
+        if os.path.exists(program_path) and os.access(program_path, os.X_OK):
+            print(f"{program} is {program_path}")
+        else:
+            print(f"{program}: not found")
+    else: 
+        print(f"test, program = {program} flop ")
+        print(f"{program}: not found")
+        
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
-            break
-        elif command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            cmd = command[5:]
-            if cmd in BUILDINS:
-                print(f"{cmd} is a shell builtin")
-            elif PATH:
-                for dir in PATH.split(os.pathsep):
-                    full_path = os.path.join(dir, cmd)
-                    print(full_path)
-                    
-                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                        print(f"{cmd} is {full_path}")
-                        break
-                else:
-                    print(f"{cmd}: not found")
-            else:
-                print(f"{cmd}: not found")
+
+        line = input()
+
+        for command in commands:
+            if line.startswith(command):
+                commands[command](line)
+                break
         else:
-            print(f"{command}: command not found")
+            sys.stderr.write(f"{line}: command not found\n")
 
 
 if __name__ == "__main__":
