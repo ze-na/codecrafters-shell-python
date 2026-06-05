d366e20187bcabc2db6836bf09056f00d196f7c6
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-05 13:05:09 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,23 +6,56 @@ commands = {
     "echo": lambda line: print(line[5:]),
     "type": lambda line: print(f"{args} is a shell builtin")
     if (args := "".join(line.split()[1:])) in commands
-    else find_dir(line[5:])
+    else find_type(line[5:])
 }
+
+def run_external(line):
+    input = line.split()
+    program = input[0]
+    found, program_path = program_found(program)
+    if found:
+        for arg in input[1:]:
+        print(f"printing arg: {arg}")
+    else:    
+    sys.stderr.write(f"{line}: command not found\n")
+
+        
 
 def get_path_directories():
     return os.environ['PATH'].split(os.pathsep)
-
-def find_dir(program):
+        
+def program_found(program):
     directories = get_path_directories()
+    
+    program_path = ""
 
     for directory_path in directories:
         program_path = os.path.join(directory_path, program)
         
         if os.path.exists(program_path) and os.access(program_path, os.X_OK):
-            print(f"{program} is {program_path}")
-            return
+            # print(f"{program} is {program_path}")
+            return True, program_path
     else: 
+        return False, program_path
+        # print(f"{program}: not found")
+
+def find_type(program):
+    found, program_path = program_found(program)
+    if found:
+        print(f"{program} is {program_path}")
+    else:
         print(f"{program}: not found")
+        
+    # directories = get_path_directories()
+
+    # for directory_path in directories:
+    #     program_path = os.path.join(directory_path, program)
+        
+    #     if os.path.exists(program_path) and os.access(program_path, os.X_OK):
+    #         print(f"{program} is {program_path}")
+    #         return
+    # else: 
+    #     print(f"{program}: not found")
         
 
 def main():
@@ -36,7 +69,8 @@ def main():
                 commands[command](line)
                 break
         else:
-            sys.stderr.write(f"{line}: command not found\n")
+            run_external(line)
+            # sys.stderr.write(f"{line}: command not found\n")
 
 
 if __name__ == "__main__":
