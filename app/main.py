7925b2c350af51d40e914d7adb1fb148a256497f
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-07 16:25:19 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -18,8 +18,8 @@ def run_external(line):
     
     # program_composition = input.append(program_path)
     if found:
-        for arg in input[1:]:    
-            program_composition = program_composition + " " + arg
+        # for arg in input[1:]:    
+        #     program_composition = program_composition + " " + arg
         # os.subprocess.run(["ls", "-l", "/dev/null"], capture_output=True)
         print(f"pprogram_compostition: {program_composition}")    
         subprocess.run(program_composition, capture_output=True)
