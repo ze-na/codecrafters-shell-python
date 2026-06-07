31ea4fd77fd6c5e8ca253a85ddb9978de2740177
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-07 16:22:29 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 
 commands = {
     "exit": lambda line: sys.exit(0),
@@ -21,7 +22,7 @@ def run_external(line):
             print(f"printing arg: {arg}")        
             program_composition = program_composition + " " + arg
         # os.subprocess.run(["ls", "-l", "/dev/null"], capture_output=True)
-        os.subprocess.run(program_composition, capture_output=True)
+        subprocess.run(program_composition, capture_output=True)
     else:    
         sys.stderr.write(f"{line}: command not found\n")
 
