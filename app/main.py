81e37f4406174ef6482a0a111f33e067cca81d63
Z.M.A.A.Elghlan@student.tudelft.nl <Z.M.A.A.Elghlan@student.tudelft.nl>
2026-06-14 04:13:37 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def run_external(line):
         #     program_composition = program_composition + " " + arg
         # os.subprocess.run(["ls", "-l", "/dev/null"], capture_output=True)
         print(f"pprogram_compostition: {program_composition}")    
-        subprocess.run(program_composition, capture_output=True)
+        subprocess.run(input, capture_output=True)
     else:    
         sys.stderr.write(f"{line}: command not found\n")
 
